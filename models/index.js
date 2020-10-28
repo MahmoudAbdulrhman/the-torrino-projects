2a4a3d8e6ed1d39109ecba633f1a3f19d43ce6bf
daejo <daejo@icloud.com>
2020-10-27 18:37:29 -0700

    creates thumbsup linking

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 const Question = require('./Question');
 const User = require('./User');
 const Answer = require('./Answer');
+const Thumbsup = require('./Thumbsup');
 
 // ==== USER ASSOCIATIONS ==== //
 User.hasMany(Question, {
@@ -30,5 +31,34 @@ Answer.belongsTo(Question, {
   foreignKey: 'question_id'
 });
 
+// ==== THUMBSUP ASSOCIATIONS ==== //
+User.belongsToMany(Question, {
+    through: Thumbsup,
+    as: 'thumbsuped_questions',
+    foreignKey: 'user_id'
+  });
+  
+Question.belongsToMany(User, {
+through: Thumbsup,
+    as: 'thumbsuped_questions',
+    foreignKey: 'question_id'
+});
 
-module.exports = { User, Question, Answer };
+Thumbsup.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Thumbsup.belongsTo(Question, {
+    foreignKey: 'question_id'
+});
+
+User.hasMany(Thumbsup, {
+    foreignKey: 'user_id'
+});
+
+Question.hasMany(Thumbsup, {
+    foreignKey: 'question_id'
+});
+
+
+module.exports = { User, Question, Answer, Thumbsup };
