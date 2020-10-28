2c3920e8649ebae06d8fab806587af9d4ce294d5
daejo <daejo@icloud.com>
2020-10-27 18:06:20 -0700

    adds question model

---
+++ models/Question.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Question model
+class Question extends Model {}
+ 
+// create fields/columns for Question model
+Question.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'question'
+  }
+);
+
+module.exports = Question;
