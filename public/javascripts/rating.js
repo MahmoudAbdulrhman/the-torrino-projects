702df313631ad0936b86ef2b55632212747e3724
daejo <daejo@icloud.com>
2020-11-01 04:28:13 -0800

    fixes linking

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,43 +1,43 @@
 
-async function ratingHandler(event) {
-    event.preventDefault();
+// async function ratingHandler(event) {
+//     event.preventDefault();
 
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+//     const id = window.location.toString().split('/')[
+//         window.location.toString().split('/').length - 1
+//     ];
 
-    const response = await fetch('/api/posts/rating', {
-        method: 'PUT',
-        body: JSON.stringify({
-            post_id: id
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+//     const response = await fetch('/api/posts/rating', {
+//         method: 'PUT',
+//         body: JSON.stringify({
+//             post_id: id
+//         }),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     });
 
     
 
-    if (response.ok) {
-        document.location.reload();
-    } else {
-        alert(response.statusText);
-    }
-}
+//     if (response.ok) {
+//         document.location.reload();
+//     } else {
+//         alert(response.statusText);
+//     }
+// }
 
 const container = document.querySelector('.rating');
 const items = container.querySelectorAll('.rating-item')
 
-container.onClick = e => {
+container.onclick = e => {
     const elClass = e.target.classList;
     if (!elClass.contains('active')) {
-    items.forEach(
-        item => item.classList.remove('active')
-    );
-    console.log(e.target.getAttribute("data-rate"));
-    elClass.add('active');
+        items.forEach(
+            item => item.classList.remove('active')
+        );
+        console.log(e.target.getAttribute("data-rate"));
+        elClass.add('active');
     }
-}
+};
 
 
 // document.querySelector('.rating-btn').addEventListener('click', ratingHandler);
