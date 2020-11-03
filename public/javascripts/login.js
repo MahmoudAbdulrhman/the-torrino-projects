bef2585b1e7b1869f5e778b1a92ed2e2fd030899
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-11-02 17:20:19 -0800

    update login

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,7 +1,7 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
     if (username && password) {
--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -3,12 +3,14 @@ async function signupFormHandler(event) {
 
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
 
     if (username  && password) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
+                email,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
