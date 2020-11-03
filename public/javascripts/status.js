81f0ffc8d1e7570869c0fe53035fce91df8981ac
daejo <daejo@icloud.com>
2020-11-03 04:58:54 -0800

    adds status javascript

--- public/javascripts/status.js
+++ public/javascripts/status.js
@@ -5,35 +5,30 @@ async function statusHandler(event) {
         window.location.toString().split('/').length - 1
     ];
 
-    const container = document.querySelector('.status');
-    const items = container.querySelectorAll('.status-item');
+    const container = document.querySelector('.close');
 
     container.onclick = e => {
         const elClass = e.target.classList;
-        if (!elClass.contains('active')) {
-            items.forEach(
-                item => item.classList.remove('active')
-            );
-            console.log(e.target.getAttribute("data-rate"));
-            elClass.add('active');
+        
+        const response = await fetch('/api/posts', {
+            method: 'PUT',
+            body: JSON.stringify({
+                post_id: id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
         }
+        
     };
 
-    const response = await fetch('/api/posts', {
-        method: 'PUT',
-        body: JSON.stringify({
-            post_id: id
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-
-    if (response.ok) {
-        document.location.reload();
-    } else {
-        alert(response.statusText);
-    }
+    
 }
 
 document.querySelector('.status-btn').addEventListener('click', statusHandler);
