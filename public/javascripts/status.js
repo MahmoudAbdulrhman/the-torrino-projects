fa520df676c5bd79bd4758ba417830f0980fc5be
daejo <daejo@icloud.com>
2020-11-04 01:43:09 -0800

    fixes status update

--- public/javascripts/status.js
+++ public/javascripts/status.js
@@ -1,33 +1,22 @@
-async function statusHandler(event) {
-    event.preventDefault();
-
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
-
-    const container = document.querySelector('.close');
-
-    container.onclick = e => {
-        const elClass = e.target.classList;
-        if (!elClass.contains('status')) {
-            const response = await fetch('/api/posts', {
+        const container = document.querySelector('.close-btn');
+        container.onclick = e => {
+            const response = fetch(`/api/questions/${id}`, {
                 method: 'PUT',
                 body: JSON.stringify({
-                    post_id: id,
+                    question_id: id,
                     status: false
                 }),
                 headers: {
                     'Content-Type': 'application/json'
                 }    
-            });
+            })
+        }
 
-            if (response.ok) {
-                document.location.reload();
-            } else {
-                alert(response.statusText);
-            }
-        } 
-    };  
-}
-
-document.querySelector('.close-btn').addEventListener('click', statusHandler);
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
