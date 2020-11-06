388d3f87a8b88adaf0c890778143ba3c14839415
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-11-05 22:56:35 -0800

    add basic styling

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,39 +1,28 @@
-async function ratingHandler(event) {
+
+async function upvoteClickHandler(event) {
     event.preventDefault();
-
+  
     const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
+      window.location.toString().split('/').length - 1
     ];
-
-    const container = document.querySelector('.rating');
-    const items = container.querySelectorAll('.rating-item');
-
-    container.onclick = e => {
-        const elClass = e.target.classList;
-        if (!elClass.contains('active')) {
-            items.forEach(
-                item => item.classList.remove('active')
-            );
-            console.log(e.target.getAttribute("data-rate"));
-            elClass.add('active');
-        }
-    };
-
-    const response = await fetch('/api/posts/rating', {
-        method: 'PUT',
+  
+    const response = await fetch('/api/questions/rating', {
+        method: 'PATCH',
         body: JSON.stringify({
-            post_id: id
+          question_id: id
         }),
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         }
-    });
-
-    if (response.ok) {
+      });
+      
+      if (response.ok) {
         document.location.reload();
-    } else {
+      } else {
         alert(response.statusText);
-    }
-}
-
-document.querySelector('.rating-btn').addEventListener('click', ratingHandler);
+      }
+      
+  }
+  
+  
+  document.querySelector('.star').addEventListener('click', upvoteClickHandler);
