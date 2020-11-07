d4047b8348a82f5ed67977702e8562f9f9ecc79f
Marrione Nguyen <mtiamsic@gmail.com>
2020-11-06 23:14:54 -0800

    fixed add post, add comment, login, dashboard

--- public/javascripts/answer.js
+++ public/javascripts/answer.js
@@ -1,7 +1,7 @@
 async function answerFormHandler(event) {
     event.preventDefault();
 
-    const answer_text = document.querySelector('textarea[name="answer-body"]').value.trim();
+    const answer_text = document.querySelector('input[name="answer-body"]').value.trim();
 
     const question_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
